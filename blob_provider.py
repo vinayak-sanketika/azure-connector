64b81a214b4d605beca67157bb7fbfeb01a06b93
vinayak-sanketika <vinayakad@sanketika.in>
2024-05-08 11:42:16 +0530

    Updated commit

--- blob_provider.py
+++ blob_provider.py
@@ -1,8 +1,18 @@
 from abc import ABC, abstractmethod
-from object_info import ObjectInfo
+from object_info import ObjectInfo, Tag
 from typing import List,Dict,Any
+from pyspark.sql import SparkSession, DataFrame
 
 class BlobProvider(ABC):
     @abstractmethod
-    def fetch_objects(self,connectorConfig: Dict[str, Any] = None) -> List[ObjectInfo]:
+    def fetch_objects(self,config: Dict[str, Any] = None) -> List[ObjectInfo]:
+        pass
+    @abstractmethod
+    def read_object(self,config: Dict[str, Any] = None, sc: SparkSession = None) -> DataFrame:
+        pass
+    @abstractmethod
+    def get_blob_tags(self,) -> List[Tag]:
+        pass
+    @abstractmethod
+    def set_blob_tag(self,tags:List[Tag],) -> bool:
         pass
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 from typing import Dict, List, Any
 from azure.storage.blob import ContainerClient, BlobClient
+from pyspark.sql import DataFrame, SparkSession
 from blob_provider import BlobProvider
 from object_info import ObjectInfo,Tag
+from pyspark.conf import SparkConf
 import json
+
+
 
 
 class AzureBlobStorage(BlobProvider):
@@ -11,7 +15,7 @@ class AzureBlobStorage(BlobProvider):
         with open(config_file_path, "r") as config_file:
             config = json.load(config_file)
 
-        
+        self.config=config
         self.account_name = config["credentials"]["account_name"]
         self.account_key = config["credentials"]["account_key"]
         self.container_name = config["containername"]
@@ -19,6 +23,7 @@ class AzureBlobStorage(BlobProvider):
 
         self.connection_string = f"DefaultEndpointsProtocol=https;AccountName={self.account_name};AccountKey={self.account_key};BlobEndpoint={self.blob_endpoint}" 
 
+   
     def fetch_objects(self) -> List[ObjectInfo]:
         print("inside fetch_objects")
        
@@ -33,27 +38,42 @@ class AzureBlobStorage(BlobProvider):
             blob_location = f"https://{account_name}.blob.core.windows.net/{self.container_name}/{obj}"
             properties = blob_client.get_blob_properties()
             
-            print("prop",obj)
+            #print("prop",obj)
             object_info = ObjectInfo(
                name=obj.split(".")[0],
                file_size_kb=properties.size,
                location= blob_location,
                type=obj.split(".")[-1],
-               tags= self._get_blob_tags(obj)
+               tags= self.get_blob_tags(obj)
 
 
          )
             objects_info.append(object_info.to_json())
-        print(objects_info)
+        #print(objects_info)
         return objects_info
     
+    def read_object(self, object_path: str, sc: SparkSession, file_format: str) -> DataFrame:
+        try:
+            if file_format=="csv":
+                df = sc.read.format("csv").option("header",True).load(object_path)
+                df.show(5)
+        
+                return df
+            elif file_format=="json":
+                df = sc.read.format("json").option("multiLine", True).load(object_path)
+                df.show(truncate=False)
+                return df
+            else:
+                raise Exception("UNSUPPORTED_FILE_FORMAT", f"unsupported file format: {file_format}")
+        except Exception as e:
+             print(f"Failed to read data from Blob {e}")
         
 
 
     def _list_blobs_in_container(self, container_name):
         try:
             container = ContainerClient.from_connection_string(
-                conn_str=self.connection_string, container_name=container_name
+                conn_str=self.connection_string, container_name=self.container_name
             )
             if container.exists():
                 print("Container exists")
@@ -75,6 +95,13 @@ class AzureBlobStorage(BlobProvider):
             print("Exception:", ex)
             ##
         return blob_metadata
+
+    def _get_spark_session(self):
+        spark = SparkSession.builder\
+            .appName("ObsrvAzureBlobStoreConnector")\
+            .config("spark.jars.packages", "org.apache.hadoop:hadoop-azure:3.3.1") \
+            .getOrCreate()
+        return spark
 
     def _get_properties(self, container_name, blob_name):
         try:
@@ -100,10 +127,12 @@ class AzureBlobStorage(BlobProvider):
         except Exception as ex:
             print("Exception:", ex)
 
-    def _get_blob_tags(self, blob_name) -> List[Tag]:
+    def get_blob_tags(self, object_path:str) -> List[Tag]:
+        obj=object_path.split("//")[-1].split("/")[-1]
+        print("obj :",obj)
         try:
             blob_client = BlobClient.from_connection_string(
-                conn_str=self.connection_string, container_name=self.container_name, blob_name=blob_name
+                conn_str=self.connection_string, container_name=self.container_name, blob_name=obj
             )
             tags = blob_client.get_blob_tags()
             print("Blob tags:")
@@ -114,16 +143,19 @@ class AzureBlobStorage(BlobProvider):
             #return tag_objects
             
         except (ValueError, IOError) as e:
+            print("helo")
             print(f"Error retrieving tags: {e}")
         except Exception as ex:
+            print("hi")
             print("Exception:", ex)
         return [Tag(k,v) for k,v in tags.items()]
     
 
-    def _set_blob_tags(self, tags):
+    def set_blob_tag(self,object_path:str, tags) -> bool:
         try:
+            obj=object_path.split("//")[-1].split("/")[-1]
             blob_client = BlobClient.from_connection_string(
-                conn_str=self.connection_string, container_name=self.container_name, blob_name=blob_name
+                conn_str=self.connection_string, container_name=self.container_name, blob_name=obj
             )
             existing_tags = blob_client.get_blob_tags() or {}
             existing_tags.update(tags)
@@ -146,9 +178,30 @@ blob_name = "transform.txt"
 blob_storage.fetch_objects()
 
 print(" ")
-blob_storage._get_blob_tags(blob_name)
+#blob_storage.get_blob_tags(blob_name)
 
 # Updateing the tags:
-new_tags = {"Dept":"Tech" }
-blob_storage._set_blob_tags(new_tags)
-blob_storage._get_blob_tags(blob_name)
+new_tags = {"apt":"testing" }
+
+
+
+object_path="wasb://vinstore@storageemulator/unemployment.csv"
+file_format="csv"
+obj_file_type=object_path.split(".")[1]
+try:
+    if file_format!=obj_file_type:
+       raise Exception(f"File_format {file_format} not matching with the blob file type {obj_file_type}")
+    
+    else:
+        spark = blob_storage._get_spark_session()
+        blob_storage.read_object(object_path,spark,file_format)
+except Exception as e:
+    print("Exception:",e)
+blob_storage.get_blob_tags(object_path)
+blob_storage.set_blob_tag(object_path,new_tags)
+blob_storage.get_blob_tags(object_path)
+
+#blob_storage.get_spark_config()
+#blob_storage.read_object(object_path,file_format,SparkSession)
+#blob_storage._get_spark_session()
+# blob_storage.read_object(object_path,spark,file_format)
